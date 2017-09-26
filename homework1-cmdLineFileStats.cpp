d9d3d323e474aa355a8ca747419f02eaacd068e6
mattkobi789 <mattkobi789@gmail.com>
2017-09-26 01:30:38 -0400

    Almost compiles except for the sort method

--- homework1-cmdLineFileStats.cpp
+++ homework1-cmdLineFileStats.cpp
@@ -9,192 +9,197 @@ using namespace std;
 
 void bubbleSort(vector<string>)
 {
-    
+
 }
 
 int main(int argc, char *argv[])
 {
-	ifstream inFile;
-	istringstream line;
-	map<string, int> wordCount;
-	map<int, vector<string>> byCount;
-	string word = "";
-	string sLine = "";
-	int longestLine = 0;
-	int longestWord = 0;
-	int numLongestLines = 0;
-	int numFiles = 0;
-	int numLongestWord = 0;
-	int length = 0;
+    ifstream inFile;
+    istringstream line;
+    map<string, int> wordCount;
+    map<int, vector<string>> byCount;
+    string word = "";
+    string sLine = "";
+    int longestLine = 0;
+    int longestWord = 0;
+    int numLongestLines = 0;
+    int numFiles = 0;
+    int numLongestWord = 0;
+    int length = 0;
     int subLength = 0;
-	bool mFlag = false;
-	bool cFlag = false; //initialization of the
-	bool bFlag = false; //possible flags
-	bool xFlag = false;
-	//a flag is an argument whose first character is a dash
-	//so any file names or paths that start with a dash are
-	//considered flags
-	//therefore I do not have to check for the
-	//case in which a directory or file's name starts with a -
-	for (int j = 1; j < argc; j++)
-	{
-		if (argv[j][0] == '-')
-		{
-			switch (argv[j][1])
-			{
-			case 'm':
-				mFlag = true;
-				break;
-			case 'c':
-				cFlag = true;
-				break;
-			case 'b':
-				bFlag = true;
-				break;
-				//if the flag is unrecognized
-			default: //xFlag set to false, string printed
-					 //then the loops break and return main
-				xFlag = true;
-				cout << argv[j] << " UNRECOGNIZED FLAG";
-				break;
-			}
-		}
-		else
-		{
-			numFiles++;
-		}
-		if (xFlag) //stops the main loop
-		{
-			break;
-		}
-	}
-	if (xFlag) // returns main and terminates the program safely
-	{
-		return 0;
-	}
-	else if (numFiles == 0)
-	{
-		cout << "NO FILES" << endl;
-		return 0;
-	}
-	for (int i = 1; i < argc; i++)
-	{
-		if (argv[i][0] != '-')
-		{
-			inFile.open(argv[i]);
-			if (inFile.is_open())
-			{
-				while (getline(inFile, sLine))
-				{
-                    if(sLine == "")
+    int size = 0;
+    int q = 0;
+    bool mFlag = false;
+    bool cFlag = false; //initialization of the
+    bool bFlag = false; //possible flags
+    bool xFlag = false;
+    //a flag is an argument whose first character is a dash
+    //so any file names or paths that start with a dash are
+    //considered flags
+    //therefore I do not have to check for the
+    //case in which a directory or file's name starts with a -
+    for (int j = 1; j < argc; j++)
+    {
+        if (argv[j][0] == '-')
+        {
+            switch (argv[j][1])
+            {
+            case 'm':
+                mFlag = true;
+                break;
+            case 'c':
+                cFlag = true;
+                break;
+            case 'b':
+                bFlag = true;
+                break;
+                //if the flag is unrecognized
+            default: //xFlag set to false, string printed
+                     //then the loops break and return main
+                xFlag = true;
+                cout << argv[j] << " UNRECOGNIZED FLAG";
+                break;
+            }
+        }
+        else
+        {
+            numFiles++;
+        }
+        if (xFlag) //stops the main loop
+        {
+            break;
+        }
+    }
+    if (xFlag) // returns main and terminates the program safely
+    {
+        return 0;
+    }
+    else if (numFiles == 0)
+    {
+        cout << "NO FILES" << endl;
+        return 0;
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] != '-')
+        {
+            inFile.open(argv[i]);
+            if (inFile.is_open())
+            {
+                while (getline(inFile, sLine))
+                {
+                    if (sLine == "")
                     {
                         continue;
                     }
-					length = inFile.gcount();
-					line.str(sLine);
-					if (length > longestLine)
-					{
-						longestLine = length;
-						numLongestLines = 1;
-					}
-					else if (length == longestLine)
-					{
-						numLongestLines++;
-					}
-					while (line >> word)
-					{
-                        while(word = "")
+                    length = inFile.gcount();
+                    line.str(sLine);
+                    if (length > longestLine)
+                    {
+                        longestLine = length;
+                        numLongestLines = 1;
+                    }
+                    else if (length == longestLine)
+                    {
+                        numLongestLines++;
+                    }
+                    while (line >> word)
+                    {
+                        while (word == "")
                         {
                             subLength++;
                         }
-						if ((word.length()) == (unsigned)longestWord)
-						{
-							wordCount[word]++;
-						}
-						else if ((word.length()) > (unsigned)longestWord)
-						{
-							wordCount.erase(wordCount.begin(), wordCount.end());
-							wordCount[word]++;
-							longestWord = word.length();
-						}
-
-					}
-                    if(bFlag)
+                        if ((word.length()) == (unsigned)longestWord)
+                        {
+                            wordCount[word]++;
+                        }
+                        else if ((word.length()) > (unsigned)longestWord)
+                        {
+                            wordCount.erase(wordCount.begin(), wordCount.end());
+                            wordCount[word]++;
+                            longestWord = word.length();
+                        }
+                    }
+                    if (bFlag)
                     {
                         subLength--;
                         longestLine -= subLength;
                     }
-				}
-				inFile.close();
-			}
-			else
-			{
-				cout << argv[i] << " FILE NOT FOUND" << endl;
-			}
-		}
-		else
-		{
-			continue;
-		}
-		cout << argv[i] << ":" << endl;
-		if (mFlag)
-		{
+                }
+                inFile.close();
+            }
+            else
+            {
+                cout << argv[i] << " FILE NOT FOUND" << endl;
+            }
+        }
+        else
+        {
+            continue;
+        }
+        cout << argv[i] << ":" << endl;
+        if (mFlag)
+        {
             map<string, int>::iterator cit;
-			for (cit = wordCount.begin(); cit != wordCount.end(); cit++)
-			{
-				//goes to the map index for the count of the word
-				//then adds the word to the vector at that index
-				byCount[cit->second].push_back(cit->first);
-			}
-			//gets the last key in the map which should be the highest occurring words
-			map<int, vector<string>>::reverse_iterator sit = byCount.rbegin();
-			//assigns the quantity of highest occurring words to numLongestWord
-			numLongestWord = sit->first;
+            for (cit = wordCount.begin(); cit != wordCount.end(); cit++)
+            {
+                //goes to the map index for the count of the word
+                //then adds the word to the vector at that index
+                byCount[cit->second].push_back(cit->first);
+            }
+            //gets the last key in the map which should be the highest occurring words
+            map<int, vector<string>>::reverse_iterator sit = byCount.rbegin();
+            //assigns the quantity of highest occurring words to numLongestWord
+            numLongestWord = sit->first;
             //trims the vector at that key location
             byCount[numLongestWord].shrink_to_fit();
-			//sorts the vector at that key location
+            //sorts the vector at that key location
             sort(byCount[numLongestWord].begin(), byCount[numLongestWord].end());
             vector<string>::iterator qit = byCount[numLongestWord].begin();
-            while(qit != byCount.end()){
-                if((qit++) != byCount.end())
+            size = byCount[numLongestWord].size();
+            q = 0;
+            while (qit != byCount[numLongestWord].end()) {
+                if (q < size - 1)
                 {
-                    if(cFlag)
+                    if (cFlag)
                     {
-                        cout << qit->first << " (" << numLongestWord << "), ";
+                        cout << *qit << " (" << numLongestWord << "), ";
                     }
                     else
                     {
-                        cout << qit->first << ", ";
+                        cout << *qit << ", ";
                     }
                 }
                 else
                 {
-                    if(cFlag)
+                    if (cFlag)
                     {
-                        cout << qit->first << " (" numLongestWord << ")";
+                        cout << *qit << " (" << numLongestWord << ")";
                     }
                     else
                     {
-                        cout << qit->first;
+                        cout << *qit;
                     }
                 }
+                q++;
+                qit++;
                 cout << endl;
             }
-		}
-		else
-		{
-			map<string, int>::iterator it;
-			for (it = wordCount.begin(); it != wordCount.end(); it++)
-			{//sorted and comma-separated longest words
-				cout << it->first << ", ";
-			}
-		}
-		cout << endl;
-		cout << longestLine;
-		if (cFlag)
-		{
-			cout << " (" << numLongestLines << ")";
-		}
-		cout << endl;
-	}
+        }
+        else
+        {
+            map<string, int>::iterator it;
+            for (it = wordCount.begin(); it != wordCount.end(); it++)
+            {//sorted and comma-separated longest words
+                cout << it->first << ", ";
+            }
+        }
+        cout << endl;
+        cout << longestLine;
+        if (cFlag)
+        {
+            cout << " (" << numLongestLines << ")";
+        }
+        cout << endl;
+    }
 }
