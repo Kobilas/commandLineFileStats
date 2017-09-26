d6c066b8a2c4e6c1af047c10356db901d022b743
mattkobi789 <mattkobi789@gmail.com>
2017-09-26 00:07:54 -0400

    Fixed problem with getline() function not working

--- homework1-cmdLineFileStats.cpp
+++ homework1-cmdLineFileStats.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <fstream>
-#include <istringstream>
+#include <sstream>
 #include <string>
 #include <map>
 #include <vector>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
 	ifstream inFile;
 	istringstream line;
-	std::map<string, int> wordCount;
-	std::map<int, vector<string>> byCount;
-	std::string word = "";
+	map<string, int> wordCount;
+	map<int, vector<string>> byCount;
+	string word = "";
+	string sLine = "";
 	int longestLine = 0;
 	int longestWord = 0;
 	int numLongestLines = 0;
 	int numFiles = 0;
 	int numLongestWord = 0;
+	int length = 0;
 	bool mFlag = false;
 	bool cFlag = false; //initialization of the
 	bool bFlag = false; //possible flags
@@ -26,11 +30,11 @@ int main(int argc, char *argv[])
 	//considered flags
 	//therefore I do not have to check for the
 	//case in which a directory or file's name starts with a -
-	for (int j = 0; j < argc; j++)
+	for (int j = 1; j < argc; j++)
 	{
-		if (argv[i][0] == '-')
+		if (argv[j][0] == '-')
 		{
-			switch (argv[i][1])
+			switch (argv[j][1])
 			{
 			case 'm':
 				mFlag = true;
@@ -45,7 +49,7 @@ int main(int argc, char *argv[])
 			default: //xFlag set to false, string printed
 					 //then the loops break and return main
 				xFlag = true;
-				std::cout << argv[i] << " UNRECOGNIZED FLAG";
+				cout << argv[j] << " UNRECOGNIZED FLAG";
 				break;
 			}
 		}
@@ -57,46 +61,45 @@ int main(int argc, char *argv[])
 		{
 			break;
 		}
-		if (numFiles == 0) //also stops the main loop
-		{
-			std::cout << "NO FILES" << endl;
-			break;
-		}
 	}
-	if (xFlag || (numFiles == 0)) // returns main and terminates the program safely
+	if (xFlag) // returns main and terminates the program safely
 	{
-		return;
+		return 0;
 	}
-	for(int i = 0; i < argc; i++)
+	else if (numFiles == 0)
 	{
-		if(argv[i][0] != '-')
+		cout << "NO FILES" << endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		if (argv[i][0] != '-')
 		{
 			inFile.open(argv[i]);
-			if(inFile.is_open())
+			if (inFile.is_open())
 			{
-				while(inFile.getline())
+				while (getline(inFile, sLine))
 				{
-					line = "";
-					inFile.seekg(inFile.tellg() - inFile.gcount());
-					line(inFile.getline());
-					if(line.length() > longestLine)
+					length = inFile.gcount();
+					line.str(sLine);
+					if (length > longestLine)
 					{
-						longestLine = line.length();
+						longestLine = length;
 						numLongestLines = 1;
 					}
-					else if(line.length() == longestLine)
+					else if (length == longestLine)
 					{
 						numLongestLines++;
 					}
-					while(line >> word)
+					while (line >> word)
 					{
-						if(word.length() == longestWord)
+						if ((word.length()) == (unsigned)longestWord)
 						{
 							wordCount[word]++;
 						}
-						else if(word.length() > longestWord)
+						else if ((word.length()) > (unsigned)longestWord)
 						{
-							wordCount.erase();
+							wordCount.erase(wordCount.begin(), wordCount.end());
 							wordCount[word]++;
 							longestWord = word.length();
 						}
@@ -107,44 +110,43 @@ int main(int argc, char *argv[])
 			}
 			else
 			{
-				std::cout << argv[i] << " FILE NOT FOUND";
+				cout << argv[i] << " FILE NOT FOUND";
 			}
 		}
 		else
 		{
 			continue;
 		}
-		std::cout << argv[i] << ":" << endl;
-		if(cFlag)
+		cout << argv[i] << ":" << endl;
+		if (cFlag)
 		{
-			for (std::map<string, int>::iterator cit = wordCount.begin();
+			for (map<string, int>::iterator cit = wordCount.begin();
 				cit != wordCount.end(); cit++)
 			{
 				//goes to the map index for the count of the word
 				//then adds the word to the vector at that index
-				byCount[cit->second].push_back(it->first);
+				byCount[cit->second].push_back(cit->first);
 			}
 			//gets the last key in the map which should be the highest occurring words
-			std::map<int, vector<string>>::iterator sit = byCount.rbegin();
+			map<int, vector<string>>::reverse_iterator sit = byCount.rbegin();
 			//assigns the quantity of highest occurring words to numLongestWord
 			numLongestWord = sit->first;
 			//sorts the vector at that key location
-			byCount[numLongestWord]
 		}
 		else
 		{
-			std::map<string, int>::iterator it;
+			map<string, int>::iterator it;
 			for (it = wordCount.begin(); it != wordCount.end(); it++)
 			{//sorted and comma-separated longest words
-				std::cout << it->first << ", ";
+				cout << it->first << ", ";
 			}
 		}
-		std::cout << endl;
-		std::cout << longestline;
-		if(cFlag)
+		cout << endl;
+		cout << longestLine;
+		if (cFlag)
 		{
-			std::cout << " (" << numLongestLines << ")";
+			cout << " (" << numLongestLines << ")";
 		}
-		std::cout << endl;
+		cout << endl;
 	}
 }
